58637e7dd8d59297813a12b8afe30f6ce78e4e34
AitorJT <abluecarrotjt@gmail.com>
2024-12-01 18:09:49 +0100

    Solucion primera parte dia 1

--- Dia1/Dia1/Program.cs
+++ Dia1/Dia1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string rutaInput = "F:/Codigo/CodigoDeAdvientoPorquePolvoronesNoMeHacenFalta/Dia1/Dia1/input.txt";
+//una lista para cada columna
+List<int> listaIzq = new List<int>();
+List<int> listaDer = new List<int>();
+int distanciaTotal = 0;
+
+try{
+    foreach(string linea in File.ReadLines(rutaInput)){
+        //RemoveEmptyEntries porque hay varios espacios de separacion
+        string[] partes = linea.Split(' ', StringSplitOptions.RemoveEmptyEntries); 
+
+        listaIzq.Add(int.Parse(partes[0]));
+        listaDer.Add(int.Parse(partes[1]));
+    }
+}catch (Exception ex){
+    
+    Console.WriteLine($"Error leyendo del archivo de input: {ex.Message}");
+}
+
+listaIzq.Sort();
+listaDer.Sort();
+
+for(int i = 0; i< listaIzq.Count;i++){
+    distanciaTotal += Math.Abs(listaIzq[i] - listaDer[i]);
+}
+
+Console.WriteLine($"En teoría la solución para la distancia total debería ser esta: {distanciaTotal}");
