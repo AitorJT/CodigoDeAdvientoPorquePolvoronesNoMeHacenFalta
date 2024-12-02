78e31f9016e7a11c01a06ff9bbf768222395d6b8
AitorJT <abluecarrotjt@gmail.com>
2024-12-02 16:44:31 +0100

    ahora si

--- Dia2/Dia2/Program.cs
+++ Dia2/Dia2/Program.cs
@@ -2,96 +2,77 @@
 
 string rutaInput = "F:/Codigo/CodigoDeAdvientoPorquePolvoronesNoMeHacenFalta/Dia2/Dia2/input.txt";
 int validReports = 0;
+var sr = new StreamReader(rutaInput);
+string input = sr.ReadToEnd();
+var lineas = input.Split("\n" , StringSplitOptions.RemoveEmptyEntries);
+//var lineas = input.Split("\r\n",StringSplitOptions.RemoveEmptyEntries);
+foreach(var linea in lineas){
 
-foreach(string linea in File.ReadLines(rutaInput)){
-
+    //pasamos a lista de ints haciendo un int.parse para todos los elementos resultantes de separarlo por espacios
+    List<int> levels = linea.Split(' ').Select(int.Parse).ToList();
     
-    bool dampenerUsado = false;
-    bool reporteValido = true;
+    //foreach (var level in levels)
+    //{
+    //    Console.WriteLine(level);
+    //}
+    bool creciente = levels[0] < levels[1];
+    bool esValido = false;
+    if(creciente){
+        esValido = Ascendente(levels);
+    }else{
+        esValido = Descendente(levels);
+    }
+    
+    if(esValido){
+        validReports++;
+    }else{
+        for(int i = 0; i < levels.Count; i++ ){
+            var ignorado = levels[i];
+            levels.RemoveAt(i);
 
-    string[] levelsSplitted = linea.Split(' ', StringSplitOptions.RemoveEmptyEntries); 
-    int[] levels = Array.ConvertAll(levelsSplitted,int.Parse);
-
-    for(int i = 1; i < levels.Length; i++){
-
-        bool creciente = levels[i] > levels[i-1];
-        bool decreciente = levels[i] < levels[i-1];
-        bool incrementoValido = CalcularIncrementoValido(levels[i-1], levels[i]);
-
-        if(!incrementoValido || (!creciente && !decreciente)){
-           
-           if(dampenerUsado){
-                reporteValido = false;
-                break;  
-           }
-
-            dampenerUsado = true;
-            reporteValido = false;
-
-                
-            // Intentar omitir cada nivel una vez
-            for (int indiceIgnorado = 0; indiceIgnorado < levels.Length; indiceIgnorado++)
-            {
-                //Pido perdón
-                if (EsValidoConDampener(levels, indiceIgnorado))
-                {
-                    reporteValido = true;
-                    break;
-                }
+            creciente = levels[0] < levels[1];
+    
+            if(creciente){
+                esValido = Ascendente(levels);
+            }else{
+                esValido = Descendente(levels);
             }
 
-            if (!reporteValido) break; 
-            
+            if(esValido){
+                validReports++;
+                break;
+            }
+            //dejamos el array como estaba
+            levels.Insert(i, ignorado);
         }
     }
-
-    if(reporteValido ){
-            validReports ++;
-    }
-
-}
-//Funcion que reevalua el array pero la posicion ignorar no se evalua 
-static bool EsValidoConDampener(int[] levels, int ignorar)
-{
-    bool creciente = true;
-    bool decreciente = true;
-    int indiceAnterior = -1;
-
-    for (int i = 0; i < levels.Length; i++)
-    {
-        // Ignorar índice actual
-        if (i == ignorar){
-            continue; 
-        } 
-
-        if(indiceAnterior != -1){
-            bool incrementoValido = CalcularIncrementoValido(levels[indiceAnterior], levels[i]);
-            if (!incrementoValido){
-                return false;
-            }
-            if (levels[i] < levels[indiceAnterior]){
-                creciente = false;
-            }
-                
-            if (levels[i] > levels[indiceAnterior]){
-                decreciente = false;
-            }
-        }
-
-        indiceAnterior = i;
-        
-    }
-
-    return creciente || decreciente;
-}
-static bool CalcularIncrementoValido(int anterior, int actual){
-
-    if(Math.Abs(anterior - actual) == 0 || Math.Abs(anterior - actual) > 3){
-        return false;
-    }
-    return true;
 }
 
 Console.WriteLine($"Deberían ser {validReports} reports validos.");
 
+bool Descendente(List<int> levels){
+    //se pasa por todos los elementos ya sabiendo que tienen que descender
+    for (int i = 0; i < levels.Count - 1; i++)
+    {
+        var diferencia = Math.Abs(levels[i] - levels[i + 1]);
+        //no hace falta CalcularIncrementoValido si sbemos directamente que tiene que ser mayor que la siguiente posicion pero sin una diferencia mayor de 3
+        if (levels[i] <= levels[i + 1] || diferencia > 3)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Ascendente(List<int> levels){
+    for (int i = 0; i < levels.Count - 1; i++)
+    {
+        var diferencia = Math.Abs(levels[i] - levels[i + 1]);
+        if (levels[i] >= levels[i + 1] || diferencia > 3)
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
